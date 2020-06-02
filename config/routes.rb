5722feed55a10b335e386bcbe9d4e79747d20ef7
j-jasmin <jasmin.jenull@gmx.at>
2020-06-02 11:30:07 +0200

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'dashboard', to: 'profiles#dashboard', as: :dashboard
+
+  resources :startups, only: [ :index, :show ] do
+    resources :favorites, only: :create
+    resources :applications, only: [ :new, :create ]
+  end
+
+  resources :sectors, only: :index
+  resources :profiles, only: [ :edit, :update ]
+  resources :user_sectors, only: [ :index, :create, :edit, :update ]
 end
