5319a94cea8e2d01ad3da2a3cf43e3d11bb6f6d9
j-jasmin <jasmin.jenull@gmx.at>
2020-06-02 15:57:51 +0200

    fixed validations and added index + show for startup

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -1,7 +1,11 @@
 class StartupsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
+    @startups = Startup.all
   end
 
   def show
+    @startup = Startup.find(params[:id])
   end
 end
