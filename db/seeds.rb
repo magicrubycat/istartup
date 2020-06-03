098ea6ccf70ace07a5dbfc4713b2dd98e8065442
alon kraus <alonkrauslaw@gmail.com>
2020-06-03 11:25:39 +0200

    midpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Startup.delete_all
+Sector.delete_all
+StartupSector.delete_all
+
+#1
+startup1 = Startup.new(
+name: 'iMops',
+description: 'Cool mops that clean the house and pump water from wells'
+address: 'Rudi-Dutschke-Straße 26, Berlin 10119, Germany'
+website: 'www.imops.com'
+email: 'ceo@imops.com'
+)
+startup1.save
+
+cleaning_sector = Sector.new(
+name: 'cleaning'
+
+)
+cleaning_sector.save
+
+cleaning_startup = StartupSector.new(
+startup: start1
+sector: cleaning_sector
+
+)
+cleaning_startup.save
+
+#2
+
+startup1 = Startup.new(
+name: 'iBox',
+description: "really cool boxing clubs that beat you till you're down" 
+address: 
+website:
+email:
+)
+startup1.save
+
+sports_sector = Sector.new(
+name: 'sports'
+
+)
+sports_sector.save
+
+sports_startup = StartupSector.new(
+startup: start1
+sector: sports_sector
+
+)
+sports_startup.save
