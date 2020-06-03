30e6a36059d16675468d5956996fe5d4ec1d4477
j-jasmin <jasmin.jenull@gmx.at>
2020-06-03 13:43:30 +0200

    sector-index and user_sector update working

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,20 @@
 class ProfilesController < ApplicationController
-  def edit
-  end
+  before_action :set_user, only: :update
 
   def update
+    redirect_to startups_path if @user.update(user_params)
   end
 
   def dashboard
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(sector_ids: [])
   end
 end
