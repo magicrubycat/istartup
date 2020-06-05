c9c232d956576561a47b9fd71a4be924d306b5a6
j-jasmin <jasmin.jenull@gmx.at>
2020-06-05 14:13:14 +0200

    applications...

--- app/models/application.rb
+++ app/models/application.rb
@@ -5,6 +5,10 @@ class Application < ApplicationRecord
   belongs_to :startup
 
   def content
-    "My name is #{user.first_name} and I am super interested in applying to your wonderful eh ja"
+    "
+    #{self.startup.name}
+    #{self.startup.address} Dear Hiring Manager, I'm writing to express my interest in the Web Content Manager position listed on Monster.com. I have experience building large, consumer-focused health-based content sites. While much of my experience has been in the business world, I understand the social value of this sector and I am confident that my business experience will be an asset to your organization. My responsibilities have included the development and management of: website editorial voice and style; editorial calendars; and the daily content programming and production for various websites. I have worked closely with healthcare professionals and medical editors to help them to provide the best possible information to a consumer audience of patients. I have also helped physicians to use their medical content to write user-friendly and easily comprehensible text. Experience has taught me how to build strong relationships with all departments in an organization. I have the ability to work within a team as well as cross-team. I can work with web engineers to resolve technical issues and implement technical enhancements. I am confident working with development departments to implement design and functional enhancements, and to monitor site statistics and conduct search engine optimization. Thank you for your consideration.
+    #{self.user.first_name} #{self.user.last_name}
+    #{self.user.email}"
   end
 end
